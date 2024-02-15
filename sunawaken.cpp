92712adcad2503f058e8b15cf670b229b558ada0
Akira Flakes <102316359+Akirazin-owo@users.noreply.github.com>
2024-02-15 19:06:14 -0300

    The program itself, both the source code and compiled into linux.

---
+++ sunawaken.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    std::cout << "Hello, World!";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+    std::cin.get();
+	return 0;
+}
