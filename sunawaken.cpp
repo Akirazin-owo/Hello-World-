908e9a157aad71901b1648831be7251c8c52b6d1
sublime <jv2022tavares@gmail.com>
2024-02-16 21:53:07 -0300

    ae

--- sunawaken.cpp
+++ sunawaken.cpp
@@ -4,8 +4,7 @@
 int main()
 {
     std::cout << "Hello, World!";
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+   
     std::cin.get();
 	return 0;
 }
